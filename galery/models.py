fec91b3c27c8f0d3a10be6ee841a92a9c41360b3
Mateus Campos <mateus.camphos2000@gmail.com>
2025-03-27 10:45:43 -0300

    Add model to image and streamline parts of the app.

--- galery/models.py
+++ galery/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    caption = models.CharField(max_length=150, null=False, blank=False)
+    description = models.TextField(null=False, blank=False)
+    photograph = models.CharField(max_length=150, null=False, blank=False)
+
+    def __str__(self):
+        return f'Image [name={self.name}]'
--- galery/views.py
+++ galery/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from galery.models import Image
+
 
 def index(request):
-    return render (request, 'galery/index.html')
+    data = {
+        1: { 'name': 'Nebulosa de Carina', 'caption': 'Webtelescope.org / NASA / James Webb'},
+        2: { 'name': 'Galáxia NGC 1079', 'caption': 'nasa.org / NASA / Hubble'}
+    }
+
+    return render (request, 'galery/index.html', { 'cards': data })
 
 def image(request):
     return render(request, 'galery/image.html')
