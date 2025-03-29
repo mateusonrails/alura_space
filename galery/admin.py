5f764db3be586fdc5e91c6f9a9e334524405490b
Mateus Campos <mateus.camphos2000@gmail.com>
2025-03-29 11:24:37 -0300

    Add category to ImageModel.

--- galery/admin.py
+++ galery/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from galery.models import Image
 
-# Register your models here.
+class ListingImages(admin.ModelAdmin):
+    list_display = ('id', 'name', 'caption')
+    list_display_links = ('id', 'name')
+    search_fields = ('name',)
+
+admin.site.register(Image, ListingImages)
--- galery/models.py
+++ galery/models.py
@@ -1,8 +1,17 @@
 from django.db import models
 
 class Image(models.Model):
+
+    CATEGORIES = [
+        ('NEBULOSA', 'Nebulosa'),
+        ('ESTRELA', 'Estrela'),
+        ('GALÁXIA', 'Galáxia'),
+        ('PLANETA', 'Planeta'),
+    ]
+
     name = models.CharField(max_length=100, null=False, blank=False)
     caption = models.CharField(max_length=150, null=False, blank=False)
+    category = models.CharField(max_length=100, choices=CATEGORIES, default='')
     description = models.TextField(null=False, blank=False)
     photograph = models.CharField(max_length=150, null=False, blank=False)
 
