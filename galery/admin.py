2b13f84687eecf1c12072d8a0fc8f087e00bbba4
Mateus Campos <mateus.camphos2000@gmail.com>
2025-03-30 00:15:30 -0300

    Add new filters from Admin.

--- galery/admin.py
+++ galery/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from galery.models import Image
 
 class ListingImages(admin.ModelAdmin):
-    list_display = ('id', 'name', 'caption')
+    list_display = ('id', 'name', 'caption', 'published')
     list_display_links = ('id', 'name')
     search_fields = ('name',)
+    list_filter = ('category',)
+    list_editable = ('published',)
+    list_per_page = 10
 
 admin.site.register(Image, ListingImages)
