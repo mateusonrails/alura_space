cf2543c2b9c811c6d1e08444448e9b6be01cda4f
Mateus Campos <mateus.camphos2000@gmail.com>
2025-03-31 10:57:30 -0300

    Add search form.

--- galery/views.py
+++ galery/views.py
@@ -9,3 +9,13 @@ def index(request):
 def image(request, image_id):
     image = get_object_or_404(Image, pk=image_id)
     return render(request, 'galery/image.html', { 'image': image })
+
+def search(request):
+    images = Image.objects.order_by('date_photograph').filter(published=True)
+
+    if "search" in request.GET:
+        search_name = request.GET['search']
+        if search_name:
+            images = images.filter(name__icontains=search_name)
+
+    return render(request, 'galery/search.html', { 'cards': images })
